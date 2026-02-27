aab1dabb95ad0d629dedb219ad8cbad8a671f3b4
brayand227 <brayancalle227@gmail.com>
2026-02-27 07:37:35 -0500

    primer commit

---
+++ src/test/java/banco/co/CoApplicationTests.java
@@ -0,0 +1,13 @@
+package banco.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
